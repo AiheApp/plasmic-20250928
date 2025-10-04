d26179d05f7c847d6b4df950dc4a574bf54e1bbe
Shahar Bano <shaharbano893@gmail.com>
2025-10-04 17:48:21 +0500

    stag

--- platform/wab/src/wab/client/components/PublicLink.tsx
+++ platform/wab/src/wab/client/components/PublicLink.tsx
@@ -7,15 +7,13 @@ import { Link as ReactRouterLink } from "react-router-dom";
 type PublicLinkProps = React.ComponentProps<"a">;
 
 export function PublicLink(props: PublicLinkProps) {
-  const href = props.href;
-
-  if (!href || isAbsoluteUrl(href)) {
-    // Use normal link for absolute URLs or when href is undefined/empty
+  if (isAbsoluteUrl(props.href || "")) {
+    // Use normal link for absolute URLs
     return <a {...props} />;
+  } else {
+    // Use RR Link for internal navigation
+    return (
+      <ReactRouterLink {...(props as any)} href={undefined} to={props.href} />
+    );
   }
-
-  // Use RR Link for internal navigation
-  return (
-    <ReactRouterLink {...(props as any)} href={undefined} to={href} />
-  );
 }
