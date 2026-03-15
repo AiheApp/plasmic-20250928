04736fdc3401cd400c60e3cf82ff63e452052868
shahar bano <shaharbano893@gmail.com>
2026-03-15 14:39:45 +0500

    updated host

--- platform/wab/src/wab/shared/devflag-overrides.ts
+++ platform/wab/src/wab/shared/devflag-overrides.ts
@@ -4,4 +4,5 @@ export const DEFAULT_DEVFLAG_OVERRIDES: Partial<DevFlagsType> = {
   enablePlasmicHosting: false,
   showCopilot: true,
   globalTrustedHosts: ["http://157.90.224.29:3005"],
+  defaultHostUrl: "http://157.90.224.29:3005/static/host.html",
 };
